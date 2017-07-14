846117745368c9c41f0fe670ff0f5796f49432d3
Alice Guan <aliguan726@gmail.com>
2017-07-14 01:21:13 -0400

    Pass acces Token

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyAuthService } from '../services/spotify-auth.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Headers } from '@angular/http';
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
   styleUrls: ['./profile-view.component.css']
 })
 export class ProfileViewComponent implements OnInit {
-  private tokens: Object;
 
+ private tokens;
 
-  constructor( private spotifyauth: SpotifyAuthService, private activatedRoute: ActivatedRoute) { }
+  constructor( private spotifyauth: SpotifyAuthService,
+      private activatedRoute: ActivatedRoute) { }
+
 
   ngOnInit() {
-      this.activatedRoute.queryParams.subscribe((queryParams) => {
+      this.activatedRoute.queryParams
+      .subscribe((queryParams) => {
           const code = queryParams['code'];
           const state = queryParams['state'];
-          this.spotifyauth.callback(code, state).subscribe(res => { if (res) {  this.tokens = res; console.log(this.tokens) } } );
-      });
-
-  }
+          this.spotifyauth.callback(code, state)
+            // .subscribe(res => { this.tokens = res;
+            //     this.spotifyauth.getUser(this.tokens.access_token)
+            //         .subscribe( response => console.log(response))
+            // });
+        })
+    }
 
 }
--- src/app/services/spotify-auth.service.ts
+++ src/app/services/spotify-auth.service.ts
@@ -1,17 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class SpotifyAuthService {
+
   BASE_URL: String = 'http://localhost:8888';
 
-  constructor(private http: Http) { }
+  private tokens;
+  constructor(private http: Http) {
+
+  }
 
  login() {
-      return this.http.get(`${this.BASE_URL}/login`).map(res => res.json());
+      return this.http.get(`${this.BASE_URL}/login`)
+        .map(res => res.json());
   }
   callback(code, state) {
-      return this.http.get(`${this.BASE_URL}/callback`, {params: {code: code, state: state}}).map(res => res.json());
+    return this.http.get(`${this.BASE_URL}/callback`, {params: {code: code, state: state}})
+        .toPromise()
+        .then(res => { this.tokens = res.json() } );
   }
+
+  getUser(token) {
+      console.log(token);
+      const headers = new Headers();
+    //   headers.append('Authorization', 'Bearer' + token);
+    //   console.log(headers);
+    //   return this.http.get( 'https://api.spotify.com/v1/me', { headers: headers })
+    //     .map(res => res.json());
+  }
+
+
 }
